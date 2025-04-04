cd3cc387d6861b4d0b431df1e00d35d289633c87
Victor Ivan Padilla Ramos <prvictorivan@MacBook-Air-de-Victor.local>
2025-04-04 00:34:17 -0600

    Corrige scope inválido en login

--- pages/api/login.js
+++ pages/api/login.js
@@ -3,7 +3,7 @@
 export default function handler(req, res) {
     const redirect_uri = "https://puerto-copy-site.vercel.app/api/callback"
     const client_id = "A7ZaJoVv7FN2UkMJ7JWX"
-    const scope = "receipts.read customers.read"
+    const scope = "receipt.read customer.read"
   
     const authURL = `https://api.loyverse.com/oauth/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}`
   
