105ba420f4e60ab22399d8031b2740882363ca70
Victor Ivan Padilla Ramos <prvictorivan@MacBook-Air-de-Victor.local>
2025-04-16 00:32:44 -0500

    Agrega log de datos recibidos en la API de generar factura

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,38 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        {/* Ícono en la pestaña */}
+        <link rel="icon" href="/favicon.png" />
+
+        {/* SEO básico (opcional) */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Puerto Copy - Centro de impresiones en Puerto Vallarta" />
+        <meta property="og:title" content="Puerto Copy" />
+        <meta property="og:image" content="https://puertocopy.com/favicon.png" />
+        <meta property="og:url" content="https://puertocopy.com" />
+        <meta property="og:type" content="website" />
+
+        {/* Logo para resultados de Google (opcional, ayuda con SEO) */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              "url": "https://puertocopy.com",
+              "logo": "https://puertocopy.com/favicon.png"
+            }),
+          }}
+        />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
