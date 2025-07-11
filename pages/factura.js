319aa6c1d8d63f6ee00cd5808716abe9f62a917b
Victor Ivan Padilla Ramos <prvictorivan@MacBook-Air-de-Victor.local>
2025-07-11 08:53:54 -0600

    Redirige /facturacion a /factura

--- pages/factura.js
+++ pages/factura.js
@@ -18,6 +18,7 @@ const regimenes = [
   { value: '616', label: '616 - Sin obligaciones fiscales' },
   { value: '621', label: '621 - Incorporación Fiscal' },
   { value: '622', label: '622 - Actividades Agrícolas, Ganaderas, Silvícolas y Pesqueras' },
+  { value: '623', label: '623 - Opcional para Grupos de Sociedades' },
   { value: '626', label: '626 - Régimen Simplificado de Confianza' },
 ];
 
